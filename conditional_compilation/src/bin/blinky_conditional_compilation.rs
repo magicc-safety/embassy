8ff1b71c8dadcb143f5e4fc6b2e72947d66d5a4c
James Usevitch <james_usevitch@byu.edu>
2024-08-07 05:28:23 -0600

    Second pass

--- conditional_compilation/src/bin/blinky_conditional_compilation.rs
+++ conditional_compilation/src/bin/blinky_conditional_compilation.rs
@@ -1,4 +1,4 @@
-#![no_std]
+    j#![no_std]
 #![no_main]
 
 use defmt::*;
@@ -11,50 +11,63 @@ use embassy_stm32::time::Hertz;
 #[cfg(feature="stm32h7rs")]
 use embassy_stm32::Config;
 
+trait CustomConfig {
+    fn config() -> Self {
+        #[cfg(feature = "stm32h7")]
+        return Default::default();
+
+        #[cfg(feature = "stm32h7rs")]
+        let config = {
+            let mut config = Config::default();
+            {
+                use embassy_stm32::rcc::*;
+                config.rcc.hse = Some(Hse {
+                    freq: Hertz(24_000_000),
+                    mode: HseMode::Oscillator,
+                });
+                config.rcc.pll1 = Some(Pll {
+                    source: PllSource::HSE,
+                    prediv: PllPreDiv::DIV3,
+                    mul: PllMul::MUL150,
+                    divp: Some(PllDiv::DIV2),
+                    divq: None,
+                    divr: None,
+                });
+                config.rcc.sys = Sysclk::PLL1_P; // 600 Mhz
+                config.rcc.ahb_pre = AHBPrescaler::DIV2; // 300 Mhz
+                config.rcc.apb1_pre = APBPrescaler::DIV2; // 150 Mhz
+                config.rcc.apb2_pre = APBPrescaler::DIV2; // 150 Mhz
+                config.rcc.apb4_pre = APBPrescaler::DIV2; // 150 Mhz
+                config.rcc.apb5_pre = APBPrescaler::DIV2; // 150 Mhz
+                config.rcc.voltage_scale = VoltageScale::HIGH;
+            }
+            return config
+        };
+    }
+}
+
+impl Custom_Compilation for CustomConfig {}
+
+
+trait LEDPin {
+    fn led_pin(&self) -> PinType {
+        #[cfg(feature="stm31h7")]
+        return self.PB13;
+
+        #[cfg(feature="stm31h7rs")]
+        return self.PD9;
+    }
+}
+
+impl LEDPin for Peripherals {}
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
 
-    #[cfg(feature="stm32h7")]
-    let config = Default::default();
-
-    #[cfg(feature="stm32h7rs")]
-    let config = {
-        let mut config = Config::default();
-        {
-            use embassy_stm32::rcc::*;
-            config.rcc.hse = Some(Hse {
-                freq: Hertz(24_000_000),
-                mode: HseMode::Oscillator,
-            });
-            config.rcc.pll1 = Some(Pll {
-                source: PllSource::HSE,
-                prediv: PllPreDiv::DIV3,
-                mul: PllMul::MUL150,
-                divp: Some(PllDiv::DIV2),
-                divq: None,
-                divr: None,
-            });
-            config.rcc.sys = Sysclk::PLL1_P; // 600 Mhz
-            config.rcc.ahb_pre = AHBPrescaler::DIV2; // 300 Mhz
-            config.rcc.apb1_pre = APBPrescaler::DIV2; // 150 Mhz
-            config.rcc.apb2_pre = APBPrescaler::DIV2; // 150 Mhz
-            config.rcc.apb4_pre = APBPrescaler::DIV2; // 150 Mhz
-            config.rcc.apb5_pre = APBPrescaler::DIV2; // 150 Mhz
-            config.rcc.voltage_scale = VoltageScale::HIGH;
-        }
-        config
-    };
-
-    let p = embassy_stm32::init(config);
+    let p = embassy_stm32::init(CustomConfig::config());
     info!("Hello World!");
 
-    #[cfg(feature="stm32h7")]
-    let led_pin = p.PB14;
-
-    #[cfg(feature="stm32h7rs")]
-    let led_pin = p.PD10;
-
-    let mut led = Output::new(led_pin, Level::High, Speed::Low);
+    let mut led = Output::new(p.led_pin(), Level::High, Speed::Low);
 
     loop {
         info!("high");
